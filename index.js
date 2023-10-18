c158b016def1c7e400e0dd1a654075b6c0ab416b
wilmarp <wilmarp@uninorte.edu.co>
2023-10-17 19:33:59 -0500

    Nombres aleatorios y más

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const qrcode = require('qrcode-terminal');
+const random_name = require('node-random-name');
 
-//const { Client } = require('whatsapp-web.js');
-//const client = new Client();
+const saludos = ['Hola', 'hola', 'Buenos', 'buenos', 'buenas', 'Buenas', '¿Cómo está?', 
+'¿cómo está?', '¿Como esta?', '¿como esta?', 'como esta', 'Saludos', 'saludos', 'saluda', 'Saluda',
+'¿Cómo estás?', '¿cómo estás?', '¿Como estas?', '¿como estas?', 'como estas'];
+
+const liquidar = ['planilla', 'Planilla', 'Liquidación', 'liquidación', 'liquidar', 'Liquidar'];
+const numeros = ['0','1','2','3','4','5','6','7','8','9'];
+const noRegistrados = ['10101010','123456789','01020304'];
 
 const { Client, LocalAuth } = require('whatsapp-web.js');
 
@@ -19,16 +25,26 @@ client.on('ready', () => {
 });
 
 client.on('message', message => {
-    if (message.body.includes('Hola') || message.body.includes('buenas')) {
-        client.sendMessage(message.from, 'Hola como estas, estas comunicado con AEL, que deseas?');
+    if (saludos.some(s => message.body.includes(s))) {
+        client.sendMessage(message.from, 'Hola, mi nombre es '+random_name({ first: true})+' de Aportes en linea, ¿Como puedo ayudarte?');
     }
 });
 
 client.on('message', message => {
-    //if (message.body === 'necesito') {
-    if (message.body.includes('necesito') || message.body.includes('ayuda')) {
-        client.sendMessage(message.from, 'Con mucho gusto, ingrese a este link');
-        client.sendMessage(message.from, 'https://www.eltiempo.com');
+    if (liquidar.some(l => message.body.includes(l))) {
+        client.sendMessage(message.from, 'Con mucho gusto, ¿Podría regalarme su número de documento para validar?');
+    }
+});
+
+client.on('message', message => {
+    if (message.body.includes(numeros)) {
+        if(message.body.includes(noRegistrados)){
+            client.sendMessage(message.from, 'Lo siento, no te encuentras registrado en nuestra plataforma.');
+            client.sendMessage(message.from, 'Puedes realizar el registro ingresando a este link: https://independientes.aportesenlinea.com/');
+        }else{
+            client.sendMessage(message.from, 'Listo, ¿Tabajaste el mes completo? ¿El salario sigue siendo de $3.000.000?');
+        }
+        
     }
 });
 
